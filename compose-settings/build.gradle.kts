9c6cea90e2cf1c24c43cbdf142546ba1558753b8
Griffin Millender <griffinn.millender@gmail.com>
2023-12-14 14:46:51 -0600

    Bump to 1.2.1

--- compose-settings/build.gradle.kts
+++ compose-settings/build.gradle.kts
@@ -62,7 +62,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.gmillz"
                 artifactId = "ComposeSettings"
-                version = "1.2"
+                version = "1.2.1"
             }
         }
     }
